56a95fef59abc06681399d67c49fb7c4f08d5454
MarsPH <m.poorhamidian@gmail.com>
2024-12-08 12:30:14 -0500

    Created Basic Menu
    I duplicated the chamber level and created a scene for menu there. It is functioning but not that beautiful for now. exit works when we build the game. start the game also needs the building as it will load the scene with index 1 which is going to be the chamber

---
+++ Assets/CoreSystems/Storage/Scripts/Menu/MenuHandler.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuHandler : MonoBehaviour
+{
+    [SerializeField] GameObject settings;
+    [SerializeField] GameObject menu;
+  
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1); // loads the scene 1 that is going to be the chamber
+        
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit(); //exits the game while builded
+    }
+
+    public void OpenSettings()
+    {
+        settings.SetActive(true);// set it as true
+        menu.SetActive(false); // disable menu
+
+    }
+
+    public void CloseSettings()
+    {
+        settings.SetActive(false);// disable settings
+        menu.SetActive(true); // enable menu
+    }
+    
+}
